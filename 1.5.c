0ed6dc21d9758b51812255c0e5557ab0612550b0
Vv3n <kittinanwutthi@gmail.com>
2025-11-22 19:46:31 +0700

    Add files via upload

---
+++ 1.5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+    int number;
+    int evenSum = 0;
+    int oddSum = 0;
+
+    printf("Please Enter Your Number(0 To Stop):");
+    if(scanf("%d", &number) != 1){
+        return 1;
+    }
+
+    while(number != 0){
+        printf("Next Number(0 To Stop):");
+        if(number % 2 == 0){
+            evenSum += number;
+        }
+        else{
+            oddSum += number;
+        }
+
+        if(scanf("%d", &number) != 1){
+            break;
+        }
+
+    }
+
+    printf("Even Sum: %d\n", evenSum);
+    printf("Odd Sum: %d", oddSum);
+    return 0;
+}
