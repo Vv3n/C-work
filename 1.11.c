d2e3555ffc8b433692e5684c974ed46c36ee1eda
Vv3n <kittinanwutthi@gmail.com>
2025-11-23 21:20:49 +0700

    Add files via upload

---
+++ 1.11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(){
+    int categoryCode;
+    float price_before_vat;
+    float totalBill = 0.0;
+    float vatAmount = 0.0;
+
+    printf("Please Enter Your Price And Code (1-4)\n");
+    if(scanf("%f %d", &price_before_vat, &categoryCode) != 2){
+        return 1;
+    }
+
+    switch(categoryCode){
+        case 1:
+            totalBill = price_before_vat * 1.07;
+            vatAmount = totalBill - price_before_vat;
+            break;
+        case 2:
+            totalBill = price_before_vat;
+            vatAmount = 0.00;
+            break;
+        case 3:
+            totalBill = price_before_vat * 1.15;
+            vatAmount = totalBill - price_before_vat;
+            break;
+
+        default:
+            break;
+    }
+
+
+    if(categoryCode >= 1 && categoryCode <= 3){
+        vatAmount = totalBill - price_before_vat;
+        printf("VAT Amount: %.2f\n", vatAmount);
+        printf("Total Price: %.2f\n", totalBill);
+    }
+    else{
+        printf("Invalid Category\n");
+    }
+
+    return 0;
+}
