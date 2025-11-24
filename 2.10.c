61cd94cc3e46d52f0e80ee2628942b2337a21e16
Vv3n <kittinanwutthi@gmail.com>
2025-11-24 22:10:20 +0700

    Update 2.10.c

--- 2.10.c
+++ 2.10.c
@@ -5,13 +5,13 @@ int main(){
     float Balance, APR, interest;
     float totalPenalties = 0.0;
 
-    printf("Enter Your InitialBalance, Penalty and month\n");
+    printf("Enter Your InitialBalance, Penalty and month \n");
     if(scanf("%f %f %d", &initialBalance, &PENALTY_FEE, &N_PERIODS) != 3){
         return 1;
     }
     Balance = initialBalance;
 
-    printf("Starting Balance: %.2f\n", initialBalance);
+    printf("Starting Balance: %.2f \n", initialBalance);
 
 
     for(i = 1; i <= N_PERIODS; i++){
@@ -22,41 +22,49 @@ int main(){
         switch(cmdCode){
             case 1:
                 Balance += amount;
-                printf("--- Month %d ---\n", i);
-                printf("Deposit:[%.2f]\n", amount);
+
+                printf("--- Month %d --- \n", i);
+                printf("Deposit : [%.2f] \n", amount);
                 break;
             case 2:
                 if(amount <= Balance){
                     Balance -= amount;
-                    printf("--- Month %d ---\n", i);
-                    printf("Withdrawal: [%.2f]\n", amount);
+
+                    printf("--- Month %d --- \n", i);
+                    printf("Withdrawal : [%.2f] \n", amount);
                 }
+                
                 else{
                     totalPenalties += PENALTY_FEE;
-                    printf("--- Month %d ---\n", i);
-                    printf("WITHDRAWAL FAILED. Penalty [%.2f] applied\n",PENALTY_FEE);
+
+                    printf("--- Month %d --- \n", i);
+                    printf("WITHDRAWAL FAILED. Penalty [%.2f] applied \n",PENALTY_FEE);
                 }
                 break;
             case 3:
                 if(Balance < 1000.00){
                     APR = 0.01;
                 }
+
                 else if(Balance >= 1000.00){
                     APR = 0.025;
                 }
+
                 interest = Balance * (APR/12.0);
                 Balance += interest;
-                printf("--- Month %d ---\n", i);
-                printf("Interest:[%.2f] (Rate: [%.2f] Percent)\n", interest, APR*100);
+
+                printf("--- Month %d --- \n", i);
+                printf("Interest : [%.2f] (Rate : [%.2f] Percent) \n", interest, APR*100);
                 break;
 
             default:
-                printf("Error: Invalid Command.\n");
+                printf("Error : Invalid Command. \n");
                 break;
         }   
     }
 
-    printf("Final Balance: %.2f\n", Balance);
-    printf("Total Penalties: %.2f\n", totalPenalties);
+    printf("Final Balance : %.2f \n", Balance);
+
+    printf("Total Penalties : %.2f \n", totalPenalties);
     return 0;
 }
