72ee52360c9345db152c254eb3e164aa810dac04
Vv3n <kittinanwutthi@gmail.com>
2025-11-24 17:56:48 +0700

    Update 2.5.c

--- 2.5.c
+++ 2.5.c
@@ -3,14 +3,16 @@ int main(){
     int score;
     int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
 
-    printf("Please Enter socre:");
+    printf("Please Enter socre (0-100):");
     if(scanf("%d", &score) != 1 ){
         return 1;
     }
 
     while(score != -1){
-        printf("Please Enter socre:");
-        if(score >= 80){
+        if(score > 100){
+            printf("This score is wrong try again\n");
+        }
+        else if(score >= 80){
             countA++;
         }
         else if(score >= 70){
@@ -21,10 +23,11 @@ int main(){
         }
         else if(score >= 50){
             countD++;
-        }
+        }\
         else{
             countF++;
         }
+        printf("Please Enter socre (0-100):");
         if(scanf("%d", &score) != 1){
             break;
         }
