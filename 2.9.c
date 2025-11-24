07f0147d999e0462d79da4b43f04c71401237446
Vv3n <kittinanwutthi@gmail.com>
2025-11-24 21:34:39 +0700

    Update 2.9.c

--- 2.9.c
+++ 2.9.c
@@ -2,44 +2,41 @@
 int main(){
     int initialStock, N, cmdCode, quantity, i;
     float PENALTY_FEE;
-    int currentStock;
+    int Stock;
     float totalPenalties = 0.0;
 
     printf("Please enter quantityproduct,penalty and quantity list\n");
     if(scanf("%d %f %d", &initialStock, &PENALTY_FEE, &N) != 3){
         return 1;
     }
-    currentStock = initialStock;
+    Stock = initialStock;
 
 
     for(i = 0; i < N; i++){
-        printf("Please Enter Code and Quantity\n");
         if(scanf("%d %d", &cmdCode, &quantity) != 2){
             break;
         }
 
         switch(cmdCode){
             case 1:
-                currentStock += quantity;
+                Stock += quantity;
                 printf("Received [%d] units.\n", quantity);
                 break; 
             case 2:
                 if(quantity <= 0){
                     printf("Error: Quantity must be positive.\n");
-                    break;
                 }
-                else if(quantity <= currentStock){
-                    currentStock -= quantity;
+                else if(quantity <= Stock){
+                    Stock -= quantity;
                     printf("Shipped [%d] units.\n", quantity);
-                    break;
                 }
-                else if(quantity > currentStock){
+                else if(quantity > Stock){
                     totalPenalties += PENALTY_FEE;
                     printf("FAILURE: Insufficient stock. PENALTY [%.2f] added.\n", PENALTY_FEE);
-                    break;
                 }
+                break;
             case 3:
-                printf("Current Stock: %d\n", currentStock);
+                printf("Current Stock: %d\n", Stock);
                 printf("Total Penalties: %.2f\n", totalPenalties);
                 break;
 
